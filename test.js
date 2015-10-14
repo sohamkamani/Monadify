2248aea2c06b77107959133f69247e42be7abdc4
sohamkamani <sohamkamani@gmail.com>
2015-10-14 13:38:03 +0530

    added tests

--- test.js
+++ test.js
@@ -1,12 +1,22 @@
 var Monadify = require('./index');
-var addOne = function(n){
+var addOne = function(n) {
   return n + 1;
 };
 
 var mon1 = Monadify(1);
 var mon10 = Monadify(10);
 
-mon1.bind(addOne).bind(addOne).bind(addOne).bind(addOne).bind(console.log);
-mon10.bind(addOne).bind(addOne).bind(addOne).bind(addOne);
+mon1
+  .bind(addOne)
+  .bind(addOne)
+  .bind(addOne)
+  .bind(addOne)
+  .bind(console.log);
+
+mon10
+  .bind(addOne)
+  .bind(addOne)
+  .bind(addOne)
+  .bind(addOne);
 
 console.log(mon10.apply());
